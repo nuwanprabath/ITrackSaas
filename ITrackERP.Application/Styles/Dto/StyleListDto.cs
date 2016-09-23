eb20a008fc88aed2fc8bf4bd12df6d95aca5ab5a
chanuu.online <chanuu.online@gmail.com>
2016-09-23 17:47:39 +0530

    2016-09-2 last 2

---
+++ ITrackERP.Application/Styles/Dto/StyleListDto.cs
@@ -0,0 +1,34 @@
+﻿using Abp.Application.Services.Dto;
+using Abp.AutoMapper;
+using ITRACK.Company;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ITrackERP.Styles.Dto
+{
+
+    [AutoMapFrom(typeof(Style))]
+    public class StyleListDto : FullAuditedEntityDto<Guid>
+    {
+        public string StyleNo { get; set; }
+
+        public string OrderType { get; set; }
+
+        public  string ArticleNo { get; protected set; }
+
+        public  string Season { get; protected set; }
+
+        public  string BocNo { get; set; }
+
+        public  string ItemType { get; set; }
+
+        public  string Remark { get; protected set; }
+
+
+
+    }
+
+}
